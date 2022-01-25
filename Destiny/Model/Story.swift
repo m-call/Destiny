50e6791d5f317438413d243a4e2ebc31f1822663
Michael Callahan <mcallahan0@yahoo.com>
2022-01-25 00:53:56 -0500

    Created a Story struct with title and choice1 and choice2 constants and created and init method for the struct. Instantiated a new Story object and changed the viewDidLoad to display the story object's data.

---
+++ Destiny/Model/Story.swift
@@ -0,0 +1,22 @@
+//
+//  Story.swift
+//  Destiny
+//
+//  Created by Colleen Callahan on 1/25/22.
+//
+
+import Foundation
+
+struct Story {
+    
+    let title: String
+    let choice1: String
+    let choice2: String
+    
+    init(title: String, choice1: String, choice2: String) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+    }
+    
+}
